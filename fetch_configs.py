3a363d7ff1168b5370984b8d1d5d435b8915f487
Nathan Alderfer <alderfer.nathan@gmail.com>
2025-03-09 11:19:30 -0400

    Device config retrieval now working

---
+++ fetch_configs.py
@@ -0,0 +1,118 @@
+from dataclasses import dataclass, field
+import logging
+
+from cloudvision.Connector.codec import Wildcard
+from cloudvision.Connector.grpc_client import GRPCClient
+from cloudvision.Connector.grpc_client import create_query
+
+
+@dataclass
+class DeviceInfo:
+    hostname: str
+    serial_number: str
+
+
+def get_devices(client):
+    """
+    Get the list of devices from the analytics dataset at the /DatasetInfo/Devices path.
+    A k/v at this path is a k of the device serial and v is a map that looks like this:
+
+    {
+      "mac": "de:ad:be:ef:de:ad",
+      "status": "inactive",
+      "hostname": "notreal",
+      "modelName": "cEOSLab",
+      "deviceType": "EOS",
+      "domainName": "",
+      "eosVersion": "4.33.1F",
+      "sourceType": "",
+      "displayName": "",
+      "capabilities": [
+        "all",
+        "trafficPolicySupported"
+      ],
+      "isProvisioned": true,
+      "softwareVersion": "4.33.1F",
+      "terminAttrVersion": "v1.36.0",
+      "primaryManagementIP": "192.168.1.1",
+      "primaryManagementIPv6": ""
+    }
+
+    We want to pull hostname and device SN for any device marked as active.
+    """
+
+    # see create_query definition - first list in tuple is the path, second is the keys
+    query = [create_query(pathKeys=[(["DatasetInfo", "Devices"], [])], dId="analytics")]
+
+    devices = []
+    for batch in client.get(query):
+        for notif in batch["notifications"]:
+            serial = list(notif["updates"])[0]
+            hostname = notif["updates"][serial]["hostname"]
+            # if notif["updates"][serial]["status"] == "active":
+            if hostname == "tuf250":
+                devices.append(DeviceInfo(hostname=hostname, serial_number=serial))
+    return devices
+
+
+def get_config(client, device):
+    """
+    A device config is stored as a doubled-linked list of k/vs at /Config/running/lines in a device dataset.
+    For our purposes, we don't care about the list structure, we just want to fetch the config as a string.
+    This means we need to fetch all of the linked entries, find the head node, and traverse the list to rebuild the config as a string.
+
+    Every entry has a k of the entry id, and a v of a map with the following keys:
+
+    {
+      "next": "6e2a1a0e-c8bb-4c9a-a0ef-f4f1f3ee5908",
+      "text": "interface Ethernet2/4",
+      "previous": "c3cf581f-2da1-47db-b386-58bb8d1d81d9"
+    }
+
+    Unfortunately, we don't store an empty `previous` key for the head node, the key is just ommitted.
+    Same goes for the tail node, the `next` key is ommitted.
+    """
+    query = [create_query(pathKeys=[(["Config", "running", "lines"], [])], dId=device.serial_number)]
+
+    config_lines = {}
+    for batch in client.get(query):
+        for notif in batch["notifications"]:
+            logging.debug(f"Notif: {notif}")
+            for entry in notif["updates"]:
+                logging.debug(f"Entry: {entry}")
+                node_id = entry # mostly for my own sanity, no real need to rebind to a new var
+                next = notif["updates"][node_id].get("next")
+                previous = notif["updates"][node_id].get("previous")
+                text = notif["updates"][node_id]["text"]
+                logging.debug(f"Node ID: {node_id}, Next: {next}, Previous: {previous}, Text: {text}")
+                config_lines[node_id] = {"next": next, "previous": previous, "text": text}
+    else:
+        logging.debug(f"Config lines: {config_lines}")
+
+    # find the head node
+    for node_id, node in config_lines.items():
+        if config_lines[node_id].get("previous") is None:
+            logging.debug(f"Head node: {node_id}")
+            head_node = node_id
+            break
+    else:
+        raise Exception("No head node found")
+
+    # traverse the list to rebuild the config
+    config = []
+    current_node = head_node
+    while current_node is not None:
+        config.append(config_lines[current_node]["text"])
+        current_node = config_lines[current_node].get("next")
+    else:
+        config = "\n".join(config)
+
+    return config
+
+
+def get_configs(apiserver, access_token):
+    with GRPCClient(apiserver, token=access_token) as client:
+        devices = get_devices(client)
+        for device in devices:
+            config = get_config(client, device)
+            logging.debug(f"Config: {config}")
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import argparse
+import logging
 
+from fetch_configs import get_configs
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Haystack - Search Arista device configurations from CVaaS")
+    parser.add_argument(
+        "--log-level",
+        "-l",
+        default="INFO",
+        help="Log level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        metavar="LEVEL",
+        type=str.upper,
+    )
     subparsers = parser.add_subparsers(dest="command", help="Commands")
 
     # Fetch configs command
     fetch_parser = subparsers.add_parser("fetch-configs", help="Fetch device configurations from CVaaS")
-    fetch_parser.add_argument("--apiserver", required=True, help="CVaaS API server URL")
-    fetch_parser.add_argument("--access-token", required=True, help="CVaaS access token")
+    fetch_parser.add_argument("--apiserver", required=True, help="CVaaS API server URL", metavar="<cluster url>")
+    fetch_parser.add_argument(
+        "--access-token", required=True, help="CVaaS access token file to read", metavar="cluster.tok"
+    )
 
     # Search command
     search_parser = subparsers.add_parser("search", help="Search device configurations")
-    search_parser.add_argument("--query", required=True, help="Search query")
+    search_parser.add_argument("--query", required=True, help="Search query", action="append", dest="queries")
 
     args = parser.parse_args()
-    print(args)
+    logging.basicConfig(level=args.log_level)
+    logging.debug(f"Args: {args}")
+
+    if args.command is None:
+        parser.print_help()
+        exit(1)
+    elif args.command == "fetch-configs":
+        get_configs(args.apiserver, args.access_token)
+    elif args.command == "search":
+        search_configs(args.query)
